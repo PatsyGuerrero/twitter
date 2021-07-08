acd86ad8c47602bda4f4225f0fd3d396836971cd
patsyguerrero <patsy.guerrero95@gmail.com>
2021-07-08 10:02:33 -0500

    feat(): loginUser y autenticación

--- api/src/functions/User.js
+++ api/src/functions/User.js
@@ -1,15 +1,16 @@
 require('dotenv').config();
 const { User, Login, Retweet, Tweet } = require("../db");
-const server = require('express').Router();
-const {response} = require ('express');
+const jwt = require("jsonwebtoken");
+// const { sendEmail } = require("./emailService");
+// const { getCurrentDomainFront } = require("../config/currentDomain");
+// const server = require('express').Router();
+// const {response} = require ('express');
 
 
 async function createUser(name, password, email,user_name) {
    
     try {
-   
-      
-        
+
         let [newUser, created] = await User.findOrCreate({
           where: { email },
           defaults: {
@@ -21,37 +22,30 @@ async function createUser(name, password, email,user_name) {
           },
         });
         
-            // created true es por que lo creó, no existia
-            // siempre devuelve el usuario, pero el detalle va en función de
-            // si existia o no
-            
-            if (created){
-                return ('Ya existe el usuario con ese email');
-                
-            }
+        if (!created){
+          return ('Ya existe el usuario con ese email');     
+          }
+
           return ('El usuario fue creado')
-        //    return { __typename: "user", ...user.dataValues, detail: "Email" };
-          
-        
+              
     } catch (error) {
      return ('Error')
     }
   }
 
   async function loginUser(email, password) {
-    //
+    
     const user = await User.findOne({
       where: {
         email: email,
       },
     });
+
+    
     if (!user) {
-      return {
-        __typename: "error",
-        name: "The user doesn't exists",
-        detail: "The user doesn't exists",
-      };
+      return ('El usuario no se encuentra registrado')
     }
+    
     if (user) {
       const hashed = User.encryptPassword(password, user.salt());
       if (hashed === user.password()) {
@@ -62,22 +56,23 @@ async function createUser(name, password, email,user_name) {
           },
           "secret",
           { expiresIn: 60 * 60 }
-        ); //60*60 = 3600 seg = 1 hour
-        return {
-          __typename: "user",
+
+        ); 
+        
+        let login = await Login.create({});
+     
+        user.setLogins(login);
+   
+        return {   
           id: user.id,
           name: user.name,
           email: user.email,
           token: token,
-          role: user.role,
-          twoFA: user.twoFA
+          user_name: user.user_name
         };
+        
       } else {
-        return {
-          __typename: "error",
-          name: "invalid password",
-          detail: "invalid password",
-        };
+        return ('Contraseña incorrecta');
       }
     }
   }
