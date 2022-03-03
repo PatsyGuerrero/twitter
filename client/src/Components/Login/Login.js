aa4a39f27cd30816d977f4137353ce2ef92d1e8e
Patsy Guerrero <patsyguerreroadrian@gmail.com>
2022-03-03 11:48:37 -0500

    clear data

--- client/src/Components/Login/Login.js
+++ client/src/Components/Login/Login.js
@@ -1,118 +1,118 @@
-import React from 'react';
-import { Link, Redirect } from 'react-router-dom';
-import { urlLogin } from '../../Conections/url';
-import { FaTwitter } from 'react-icons/fa';
-// import { addCountry, deleteLittleCard, getIds } from '../../actions/actions';
-// import LittleCard from '../LittleCard/LittleCard';
-import './Login.css'; 
-// let stado = [];
+// import React from 'react';
+// import { Link, Redirect } from 'react-router-dom';
+// import { urlLogin } from '../../Conections/url';
+// import { FaTwitter } from 'react-icons/fa';
+// // import { addCountry, deleteLittleCard, getIds } from '../../actions/actions';
+// // import LittleCard from '../LittleCard/LittleCard';
+// import './Login.css'; 
+// // let stado = [];
 
-function Login() {
-    let response;
-    const [input, setInput] = React.useState({
-        email: '',
-        password: '',
-        token: false,
-        error: ''
-    });
+// function Login() {
+//     let response;
+//     const [input, setInput] = React.useState({
+//         email: '',
+//         password: '',
+//         token: false,
+//         error: ''
+//     });
 
-    // +++ ojo+++
-    const handleInputs = (e) => {
-        e.preventDefault();
-        setInput({
-            ...input,
-            [e.target.name]: e.target.value 
-        })
+//     // +++ ojo+++
+//     const handleInputs = (e) => {
+//         e.preventDefault();
+//         setInput({
+//             ...input,
+//             [e.target.name]: e.target.value 
+//         })
         
-    }
+//     }
     
-    const handleSubmit = async (e) => {
+//     const handleSubmit = async (e) => {
      
-        e.preventDefault();
+//         e.preventDefault();
         
-        response = await urlLogin(input.email, input.password);
-        // console.log(typeof response?.data, 'oooo');
-        //console.log('response data', response?.data);
-        localStorage.setItem(`token`, JSON.stringify(response.data.token));
-        localStorage.setItem(`email`, JSON.stringify(response.data.email));
-        if(typeof response?.data === 'object'){
-            setInput({
-                email: '',
-                password: '',
-                token: true,
-                error: ''
-            });
-            // redireccionar a home
-            window.dataLayer.push({'event': 'event_name'});
+//         response = await urlLogin(input.email, input.password);
+//         // console.log(typeof response?.data, 'oooo');
+//         //console.log('response data', response?.data);
+//         localStorage.setItem(`token`, JSON.stringify(response.data.token));
+//         localStorage.setItem(`email`, JSON.stringify(response.data.email));
+//         if(typeof response?.data === 'object'){
+//             setInput({
+//                 email: '',
+//                 password: '',
+//                 token: true,
+//                 error: ''
+//             });
+//             // redireccionar a home
+//             window.dataLayer.push({'event': 'event_name'});
 
-            return <Redirect to='/home' />;
-        }
-        else{
-            // cambiamos el mensaje de error
-            // errorMessage = response?.data;
-            // alert('hola')
-            setInput({
-                ...input,
-                error: response?.data
-            });
-        }
-    }
+//             return <Redirect to='/home' />;
+//         }
+//         else{
+//             // cambiamos el mensaje de error
+//             // errorMessage = response?.data;
+//             // alert('hola')
+//             setInput({
+//                 ...input,
+//                 error: response?.data
+//             });
+//         }
+//     }
 
-    // datos del componente
-    if(input.token){
-        return <Redirect to='/home'/>;
-    }
+//     // datos del componente
+//     if(input.token){
+//         return <Redirect to='/home'/>;
+//     }
 
-    return (
-        <div className='cont_principal'>
-            <Link to={`/`}>
-                <div className='cont_iconTwitter'>
-                        <FaTwitter/>
-                </div>
-            </Link>
+//     return (
+//         <div className='cont_principal'>
+//             <Link to={`/`}>
+//                 <div className='cont_iconTwitter'>
+//                         <FaTwitter/>
+//                 </div>
+//             </Link>
            
-            <h1>Login</h1>
+//             <h1>Login</h1>
 
-        <div className='cont_form'>
-            <form onSubmit={handleSubmit} >
+//         <div className='cont_form'>
+//             <form onSubmit={handleSubmit} >
 
-                    <div className="inputbox">
+//                     <div className="inputbox">
 
-                        <input 
-                        type="text"
-                        name="email"
-                        onChange={handleInputs} 
-                        value={input.email}
-                        placeholder='' 
-                        required
-                        />
-                        <span>Name</span>
+//                         <input 
+//                         type="text"
+//                         name="email"
+//                         onChange={handleInputs} 
+//                         value={input.email}
+//                         placeholder='' 
+//                         required
+//                         />
+//                         <span>Name</span>
 
-                    </div>
+//                     </div>
 
-                    <div className="inputbox">
-                        <input 
-                        type="text" 
-                        name="password" 
-                        onChange={handleInputs} 
-                        value={input.password} 
-                        placeholder='' 
-                        required
-                        />
-                        <span>Password</span>
-                    </div>
+//                     <div className="inputbox">
+//                         <input 
+//                         type="text" 
+//                         name="password" 
+//                         onChange={handleInputs} 
+//                         value={input.password} 
+//                         placeholder='' 
+//                         required
+//                         />
+//                         <span>Password</span>
+//                     </div>
 
-                    <div className="inputbox">
-                        <input type="submit"  value="Submit"/>
-                    </div>
+//                     <div className="inputbox">
+//                         <input type="submit"  value="Submit"/>
+//                     </div>
              
-                </form>
+//                 </form>
             
-            <div >{input.error}</div>
+//             <div >{input.error}</div>
 
-            </div>
-        </div>
-    );
-}
+//             </div>
+//         </div>
+//     );
+// }
 
-export default Login;
+// export default Login;
